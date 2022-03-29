80fae9c3642641576796cfc6128899dee5845efb
Ernie Hsiung <e@erniehsiung.com>
2022-03-29 15:31:09 -0400

    build: herokufy this

--- server/index.js
+++ server/index.js
@@ -14,6 +14,27 @@ app.use(morgan('dev'));
 app.use('/api/v1/warehouses', warehouseRoutes);
 app.use('/api/v1/inventories', inventoryRoutes);
 
+// Make sure you put this code snippet AFTER you define
+// your routes, but BEFORE you set up app.listen!
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+
+if (process.env.NODE_ENV === 'production') {
+  // we want make sure that express doesn't treat
+  // /client/build as a route.
+  app.use(express.static('../client/build'));
+
+  // for the most part, we want to treat incoming requests
+  // like they are API endpoints...
+  // unless... the requests are for the React App.
+  app.get('*', (req, res) => {
+    // If that's case, we'll want to load the React App
+    // directly, which will be at /client/build/index.html
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
 });
